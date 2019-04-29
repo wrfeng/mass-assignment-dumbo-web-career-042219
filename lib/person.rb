3d860ae5512e33cb1924fdd0987377ea82c5e32d
wei feng <theweifeng@gmail.com>
2019-04-29 16:40:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each {|key, val| self.send("#{key}=", val)
+    attributes.each {|key, val| self.send("#{key}=", val)}
   end
 end
